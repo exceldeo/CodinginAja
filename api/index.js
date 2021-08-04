634c45fc3dd6ff1bb39702eb62ec40c5c30e1fff
exceldeo <excel.deo99@gmail.com>
2021-08-04 09:07:36 +0700

    starter pack api localhost

--- api/index.js
+++ api/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const tasksRouter = require("./routes/tasks");
+
 const app = express();
 
-app.listen(8800, ()=>{
-    console.log("Backend server is running in port");
-})
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/api/tasks", tasksRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+
+  return;
+});
+
+
+app.listen(3000, () => console.log("Server is running at http://localhost:3000"));
---
+++ api/services/tasks.js
@@ -0,0 +1,67 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getMultiple(page = 1) {
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    `SELECT id, name FROM tasks LIMIT ?,?`, 
+    [offset, config.listPerPage]
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+async function create(name) {
+  const result = await db.query(
+    'INSERT INTO tasks(name) VALUES (?)', [name]
+  );
+
+  let message = 'Error creating task.';
+
+  if (result.affectedRows) {
+    message = 'Task created succesfully.';
+  }
+
+  return message;
+}
+
+async function update(id, name) {
+  const result = await db.query(
+    'UPDATE tasks SET name=? WHERE id=?', [name, id]
+  );
+
+  let message = 'Error updating task.';
+
+  if (result.affectedRows) {
+    message = 'Task updated succesfully.';
+  }
+
+  return message;
+}
+
+async function remove(id) {
+  const result = await db.query(
+    'DELETE FROM tasks WHERE id=?', [id]
+  );
+
+  let message = 'Error deleting task.';
+
+  if (result.affectedRows) {
+    message = 'Task deleted succesfully.';
+  }
+
+  return message;
+}
+
+module.exports = {
+  getMultiple,
+  create,
+  update,
+  remove
+}
